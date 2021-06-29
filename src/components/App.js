0a884558bf796e2778a075093daf703d1de74e92
Lucas de Morais <lucasmoraismt@gmail.com>
2021-06-29 00:23:10 -0300

    start of project

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
+
+import "../css/reset.css";
+import "../css/styles.css";
+
+import Login from "./login/Login";
+import SignUp from "./signup/SignUp";
+import Home from "./home/Home";
+import Cart from "./cart/Cart";
+
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={Login}></Route>
+          <Route path="/sign-up" exact component={SignUp}></Route>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/cart" exact component={Cart}></Route>
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
