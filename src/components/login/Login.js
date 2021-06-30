b97a9056a023b0d21d26e84a09a7c199e51f3f51
Tadeu Juliace <tadeslopes@gmail.com>
2021-06-30 00:31:10 -0300

    Layout bruto finalizado

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,3 +1,49 @@
+import styled from "styled-components"
+import Logo from "../assets/Logo.png"
+import {Redirect,Button,Form,Title,Container,Box} from "../styles/signUpAndSignIn"
+import { Link,useHistory } from "react-router-dom";
+import axios from "axios"
+import {useState} from "react";
+
 export default function Login() {
-  return;
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  let history=useHistory();
+  const [disabler,setDisabler]=useState(false);
+
+  async function signIn(e){
+    e.preventDefault();
+    setDisabler(true)
+    const body={email,password};
+    try{
+        const response=await axios.post("http://localhost:4000/login",body)
+        console.log(response.data);
+        setDisabler(false)
+        history.push('/')
+        
+    }catch(err){
+        setDisabler(false)
+        console.log('err')
+    }
+}
+  return(
+    <Container>
+      <Box>
+        <Title>
+          <img src={Logo}/>
+          <p>MoonStore</p> 
+        </Title>
+        <Form onSubmit={signIn}>
+          <p>Email</p>
+          <input type='email' value={email} onChange={e => setEmail(e.target.value)} ></input>
+          <p>Password</p>
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} ></input>
+          <Button>Sign In</Button>
+        </Form>
+        <Link to={`/sign-up`}>
+        <Redirect>First time? Create an account!</Redirect>
+        </Link>
+      </Box>
+    </Container>
+  )
 }
--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,3 +1,61 @@
+import styled from "styled-components";
+import Logo from "../assets/Logo.png";
+import { Link,useHistory } from "react-router-dom";
+import {useState} from "react";
+import {Redirect,Button,Form,Title,Container,Box} from "../styles/signUpAndSignIn"
+import axios from "axios"
 export default function SignUp() {
-  return;
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const [secPassword,setSecPassword]=useState("");
+  let history=useHistory();
+  const [disabler,setDisabler]=useState(false);
+
+  async function signUp(e){
+        e.preventDefault();
+        setDisabler(true);
+        if(password!=secPassword){
+          alert('Please confirm your password again');
+          setDisabler(false);
+          return
+        }
+        const body={name,email,password,secPassword};
+        try{
+            await axios.post("http://localhost:4000/sign-up",body)
+            setDisabler(false)
+            history.push('/login')
+        }
+        catch(err){
+            setDisabler(false)
+            console.log('err')
+        }
+    }
+
+  return(
+    <Container>
+      <Box>
+        <Title>
+          <img src={Logo}/>
+          <p>MoonStore</p> 
+        </Title>
+        <Form onSubmit={signUp}>
+          <p>Name</p>
+          <input type='text' value={name} onChange={e => setName(e.target.value)}></input>
+          <p>Email</p>
+          <input type='email' value={email} onChange={e => setEmail(e.target.value)}></input>
+          <p>Password</p>
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)}></input>
+          <p>Confirm password</p>
+          <input type='password' value={secPassword} onChange={e => setSecPassword(e.target.value)}></input>
+          <Button>Sign Up</Button>
+        </Form>
+        <Link to={`/login`}>
+        <Redirect>Switch back to log in</Redirect>
+        </Link>
+      </Box>
+    </Container>
+  );
 }
+
+
