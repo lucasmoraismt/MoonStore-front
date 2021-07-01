c7d1ffffbaf279c99ccfb11b83b38cf8109be066
Tadeu Juliace <tadeslopes@gmail.com>
2021-07-01 15:20:09 -0300

    começo da feature cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,3 +1,36 @@
+import Header from "../Header";
+import styled from "styled-components";
+
 export default function Cart() {
-  return;
+  return(
+    <>
+    <Header/>
+    <Container>
+      <BoxCart>
+       <Game>
+
+       </Game>
+      </BoxCart>
+    </Container>
+    </>
+  )
 }
+
+const BoxCart=styled.div`
+  background-color: #0F4C75;
+  width: 50%;
+  height: 500px;
+`
+const Container=styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+`
+const Game=styled.div`
+  width: 90%;
+  height: 50px;
+  background-color: white;
+  
+`
